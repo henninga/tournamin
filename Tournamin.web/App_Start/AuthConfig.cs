e529a61820b765f89d35839f5aefef4131ee4830
Henning Anderssen <hightowr@hotmail.com>
2012-11-24 09:38:44 +0100

    Removed unused packages and added raven package

--- Tournamin.web/App_Start/AuthConfig.cs
+++ Tournamin.web/App_Start/AuthConfig.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Web.WebPages.OAuth;
+//using Microsoft.Web.WebPages.OAuth;
 using Tournamin.web.Models;
 
 namespace Tournamin.web
--- Tournamin.web/Models/AccountModels.cs
+++ Tournamin.web/Models/AccountModels.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Globalization;
 using System.Web.Mvc;
@@ -9,34 +8,6 @@ using System.Web.Security;
 
 namespace Tournamin.web.Models
 {
-    public class UsersContext : DbContext
-    {
-        public UsersContext()
-            : base("DefaultConnection")
-        {
-        }
-
-        public DbSet<UserProfile> UserProfiles { get; set; }
-    }
-
-    [Table("UserProfile")]
-    public class UserProfile
-    {
-        [Key]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-    }
-
-    public class RegisterExternalLoginModel
-    {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
-
-        public string ExternalLoginData { get; set; }
-    }
-
     public class LocalPasswordModel
     {
         [Required]
@@ -89,10 +60,4 @@ namespace Tournamin.web.Models
         public string ConfirmPassword { get; set; }
     }
 
-    public class ExternalLogin
-    {
-        public string Provider { get; set; }
-        public string ProviderDisplayName { get; set; }
-        public string ProviderUserId { get; set; }
-    }
 }
