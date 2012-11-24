6f0e6e2f9bb892d1c3b07832be85f6697a161c8e
Henning Anderssen <hightowr@hotmail.com>
2012-11-24 09:18:30 +0100

    Added twitter bootstrap

--- Tournamin.web/App_Start/BundleConfig.cs
+++ Tournamin.web/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace Tournamin.web
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
@@ -23,7 +27,9 @@ namespace Tournamin.web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css")
+                                                        .Include("~/Content/bootstrap-responsive.css")
+                                                        .Include("~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
